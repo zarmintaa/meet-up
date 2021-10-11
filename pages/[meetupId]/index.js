34765907ed4963e6c37fba52dae88d3b73e71e30
HuaaXZ <59224888+zarmintaa@users.noreply.github.com>
2021-10-11 15:17:39 +0700

    get detail from database

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,44 +1,58 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from "mongodb";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <MeetupDetail
-      image="https://source.unsplash.com/random/1080x800"
-      title="A First Meetup"
-      address="Bekasi, Indonesia"
-      description="A Meetup Description"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 };
 
-export async function getStaticPaths() {
+export async function getStaticPaths(filter, options) {
+  const client = await MongoClient.connect(
+    "mongodb+srv://user1:user1@cluster0.wb1uu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+  await client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://user1:user1@cluster0.wb1uu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  await client.close();
+
   return {
     props: {
       meetupData: {
-        id: meetupId,
-        image: "https://source.unsplash.com/random/1080x800",
-        title: "A First Meetup",
-        address: "Bekasi, Indonesia",
-        description: "A Meetup Description",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
